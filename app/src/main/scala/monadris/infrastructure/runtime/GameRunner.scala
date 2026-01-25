991aca339ce7e4add267c0be9c242729544b88b6
fffclaypool <fff.claypool@gmail.com>
2026-01-25 06:48:47 +0000

    Extract runtime loop runner

--- app/src/main/scala/monadris/infrastructure/runtime/GameRunner.scala
+++ app/src/main/scala/monadris/infrastructure/runtime/GameRunner.scala
@@ -7,9 +7,6 @@ import monadris.domain.config.AppConfig
 import monadris.infrastructure.io.ConsoleService
 import monadris.infrastructure.io.TtyService
 import monadris.infrastructure.render.Renderer
-import monadris.infrastructure.runtime.Clock as GameClock
-import monadris.logic.GameLoop
-import monadris.logic.LineClearing
 import monadris.view.ScreenBuffer
 
 object GameRunner:
@@ -41,74 +38,15 @@ object GameRunner:
   def renderGameOver(state: GameState): ZIO[ConsoleService, Throwable, Unit] =
     renderer.renderGameOver(state)
 
-  private object EventLoop:
-    val CommandQueueCapacity = 100
-
-  private[infrastructure] case class LoopState(
-    gameState: GameState,
-    previousBuffer: Option[ScreenBuffer]
-  )
-
   def interactiveGameLoop(
     initialState: GameState
   ): ZIO[TtyService & ConsoleService & AppConfig, Throwable, GameState] =
     for
-      config        <- ZIO.service[AppConfig]
-      commandQueue  <- Queue.bounded[GameCommand](EventLoop.CommandQueueCapacity)
-      initialBuffer <- renderGame(initialState, config, None)
-      initialInterval = LineClearing.dropInterval(initialState.level, config.speed)
-      intervalRef    <- Ref.make(initialInterval)
-      inputFiber     <- InputStream.run(commandQueue, config.terminal.inputPollIntervalMs).fork
-      tickFiber      <- GameClock.run(commandQueue, intervalRef).fork
-      finalLoopState <- eventLoop(
-        commandQueue,
-        LoopState(initialState, Some(initialBuffer)),
+      config     <- ZIO.service[AppConfig]
+      finalState <- GameLoopRunner.interactiveGameLoop(
+        initialState,
         config,
-        intervalRef
+        renderer,
+        RandomPieceGenerator
       )
-      _ <- inputFiber.interrupt
-      _ <- tickFiber.interrupt
-    yield finalLoopState.gameState
-
-  private[infrastructure] def eventLoop(
-    queue: Queue[GameCommand],
-    state: LoopState,
-    config: AppConfig,
-    intervalRef: Ref[Long]
-  ): ZIO[ConsoleService, Throwable, LoopState] =
-    queue.take.flatMap {
-      case GameCommand.Quit =>
-        ZIO.succeed(state)
-
-      case GameCommand.UserAction(input) =>
-        handleCommand(input, state, config, intervalRef, queue)
-
-      case GameCommand.TimeTick =>
-        handleCommand(Input.Tick, state, config, intervalRef, queue)
-    }
-
-  private def handleCommand(
-    input: Input,
-    state: LoopState,
-    config: AppConfig,
-    intervalRef: Ref[Long],
-    queue: Queue[GameCommand]
-  ): ZIO[ConsoleService, Throwable, LoopState] =
-    for
-      nextShape <- RandomPieceGenerator.nextShape
-      oldState = state.gameState
-      outcome  = GameLoop.handleInput(oldState, input, nextShape, config)
-      _ <- ZIO.when(outcome.state.isGameOver && !oldState.isGameOver) {
-        ZIO.logInfo(
-          s"Game Over - Score: ${outcome.state.score}, Lines: ${outcome.state.linesCleared}, Level: ${outcome.state.level}"
-        )
-      }
-      _ <- outcome.nextIntervalMs match
-        case Some(interval) => intervalRef.set(interval)
-        case None           => ZIO.unit
-      newBuffer <- renderGame(outcome.state, config, state.previousBuffer)
-      newLoopState = LoopState(outcome.state, Some(newBuffer))
-      result <-
-        if outcome.shouldContinue then eventLoop(queue, newLoopState, config, intervalRef)
-        else ZIO.succeed(newLoopState)
-    yield result
+    yield finalState
